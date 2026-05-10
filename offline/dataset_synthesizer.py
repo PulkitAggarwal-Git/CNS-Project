91b1daf89d9c7cfb2c3c70634ae7faf8090b6045
Pulkit Aggarwal <iampulkitaggarwal@gmail.com>
2026-05-10 12:09:27 +0530

    changed file structure

---
+++ offline/dataset_synthesizer.py
@@ -0,0 +1,43 @@
+import argparse
+import os
+import multiprocessing
+import pickle
+from Dataset import get_dp, Operator
+
+SIZE = 10000
+PATH = "/kaggle/working/dataset"
+
+BATCH_SIZE = 100
+NUM_PROCESS = 10
+
+
+def multiprocess_gen_dp(p_idx):
+    '''
+    pickle_file_path = os.path.join(dataset_dir_name, str(p_idx)+".pkl")
+    with open(pickle_file_path, 'wb') as file:
+        local_dataset = [get_dp(multiple_io_sample=batch_size) for _ in range(batch_per_process)]
+        pickle.dump(local_dataset, file)
+    '''
+    
+    NUM_BATCH = SIZE // BATCH_SIZE
+    BATCH_PER_PROCESS = NUM_BATCH // NUM_PROCESS
+            
+    for idx in range(BATCH_PER_PROCESS):
+        pickle_file_path = os.path.join(PATH, str(p_idx)+"_"+str(idx)+".pkl")
+        with open(pickle_file_path, 'wb') as file:
+            local_dataset = get_dp(multiple_io_sample=BATCH_SIZE)
+            pickle.dump(local_dataset, file)
+            
+    return True
+
+
+def main():    
+    assert not os.path.exists(PATH), "path already exists!"
+    os.makedirs(PATH)
+    
+    for i in range(NUM_PROCESS):
+        multiprocess_gen_dp(i)
+    
+
+if __name__ == "__main__":
+    main()
